8e6c121bc701a7f234d172f8e307804b9be29c3d
Apolllo <apolllo@github.com>
2015-11-20 17:45:21 +0200

    some corrections

---
+++ Calc.java
@@ -0,0 +1,8 @@
+public class Calc {
+    public static void main(String[] args) {
+        Calculator.sum(2147483647,1);
+        Calculator.multiply(2147483647,2);
+        Calculator.divide(2147483647,222);
+        Calculator.substract(2147483647,-1);
+    }
+}
--- Calculator.java
+++ Calculator.java
@@ -2,27 +2,27 @@ import java.util.Arrays;
 
 public class Calculator {
 
-    public static void sum(double operand1, double operand2, double... operands) {
-        double sum = operand1 + operand2;
-        for(double operand : operands) sum += operand;
-        System.out.println("Sum is " + sum);
+    public static void presentResult(String result) {
+        System.out.println(result);
     }
 
-    public static void substract(double operand1, double operand2, double... operands) {
-        double difference = operand1 - operand2;
-        for(double operand : operands) difference -= operand;
-        System.out.println("Difference is " + difference);
+    public static void sum(int operand1, int operand2) {
+        long sum = (long)operand1 + operand2;
+        presentResult("Sum is " + sum);
     }
 
-    public static void multiply(double operand1, double operand2, double... operands) {
-        double product = operand1 * operand2;
-        for(double operand : operands) product *= operand;
-        System.out.println("Product is " + product);
+    public static void substract(int operand1, int operand2) {
+        long difference = (long)operand1 - operand2;
+        presentResult("Difference is " + difference);
     }
 
-    public static void divide(double operand1, double operand2, double... operands) {
-        double quotient = operand1 / operand2;
-        for(double operand : operands) quotient /= operand;
-        System.out.println("Quotient is " + quotient);
+    public static void multiply(int operand1, int operand2) {
+        long product = (long)operand1 * operand2;
+        presentResult("Product is " + product);
+    }
+
+    public static void divide(int operand1, int operand2) {
+        double quotient = (double)operand1 / operand2;
+        presentResult("Quotient is " + quotient);
     }
 }
