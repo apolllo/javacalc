4917774cbc39a01bd60d1845fa6c714b9cd0fdb9
Apolllo <apolllo@github.com>
2015-11-23 14:11:24 +0200

    some corrections + BigDecimal usage

--- Calculator.java
+++ Calculator.java
@@ -1,28 +1,41 @@
-import java.util.Arrays;
+import java.math.BigDecimal;
 
 public class Calculator {
 
-    public static void presentResult(String result) {
-        System.out.println(result);
+    private static void printResultToConsole(long result, String operation) {
+        switch (operation) {
+            case "add":
+                System.out.println("Sum is " + result);
+                break;
+            case "substract":
+                System.out.println("Difference is " + result);
+                break;
+            case "multiply":
+                System.out.println("Product is " + result);
+                break;
+        }
+    }
+    private static void printResultToConsole(BigDecimal result) {
+        System.out.println("Quotient is " + result.toString());
     }
 
-    public static void sum(int operand1, int operand2) {
+    public static void add(int operand1, int operand2) {
         long sum = (long)operand1 + operand2;
-        presentResult("Sum is " + sum);
+        printResultToConsole(sum, "add");
     }
 
     public static void substract(int operand1, int operand2) {
         long difference = (long)operand1 - operand2;
-        presentResult("Difference is " + difference);
+        printResultToConsole(difference, "substract");
     }
 
     public static void multiply(int operand1, int operand2) {
         long product = (long)operand1 * operand2;
-        presentResult("Product is " + product);
+        printResultToConsole(product, "multiply");
     }
 
     public static void divide(int operand1, int operand2) {
-        double quotient = (double)operand1 / operand2;
-        presentResult("Quotient is " + quotient);
+        BigDecimal quotient = new BigDecimal(operand1).divide(new BigDecimal(operand2), 10, BigDecimal.ROUND_HALF_UP);
+        printResultToConsole(quotient);
     }
 }
--- CalculatorUsage.java
+++ CalculatorUsage.java
@@ -1,6 +1,6 @@
-public class Calc {
+public class CalculatorUsage {
     public static void main(String[] args) {
-        Calculator.sum(2147483647,1);
+        Calculator.add(2147483647,1);
         Calculator.multiply(2147483647,2);
         Calculator.divide(2147483647,222);
         Calculator.substract(2147483647,-1);
